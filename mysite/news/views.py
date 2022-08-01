7c77d6cf1e8c5bde336311f7ba65c382fdef7d10
MegoM2323 <qwerty8.yfa@gmail.com>
2022-08-01 22:15:44 +0500

    17 lesson

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    context = {'news': news, 'title': 'Список новостей'}
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+        'categories': categories
+    }
     return render(request=request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, template_name='news/category.html', context={'news': news, 'categories': categories,
+                  'category': category})
